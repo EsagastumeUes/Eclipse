23bbebf8d87b3f03cc0ff6c49effe1286eb42933
EsagastumeUes <Dsagastume@Acaci.local>
2024-05-05 22:13:44 -0600

    actualizacion de SRC

---
+++ src/main/java/SM06023/SM06032/App.java
@@ -0,0 +1,19 @@
+package SM06023.SM06032;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return "Hello Word";
+	}
+	
+	
+    public static void main( String[] args )
+    {
+    	App hw = new App();
+        System.out.println(hw.Hello() );
+    }
+}
